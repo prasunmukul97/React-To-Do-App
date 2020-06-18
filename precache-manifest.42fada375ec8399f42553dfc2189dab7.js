00f53281dff23dd075fcc0a7babd0be755c7fb3f
Prasun <prasunmukul@gmail.com>
2020-06-18 22:17:08 +0530

    Updates

---
+++ precache-manifest.42fada375ec8399f42553dfc2189dab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b50cad566849481fc7f7ffacc8fc28d",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "618b4ca915db3f1b13b9",
+    "url": "/React-To-Do-App/static/css/main.a688fbf3.chunk.css"
+  },
+  {
+    "revision": "8d62943a0dbda9bb2cde",
+    "url": "/React-To-Do-App/static/js/2.c7515d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-App/static/js/2.c7515d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618b4ca915db3f1b13b9",
+    "url": "/React-To-Do-App/static/js/main.ed870137.chunk.js"
+  },
+  {
+    "revision": "74d415a30eac80f089be",
+    "url": "/React-To-Do-App/static/js/runtime-main.68a51e8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
